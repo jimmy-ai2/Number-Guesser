53f2dee738df0c8c601f2324ce2103c543cdda28
jimmy-ai2 <jichegi@gmail.com>
2026-06-06 07:46:45 +0300

    Initial commit

---
+++ number-guesser.py
@@ -0,0 +1,39 @@
+import random
+
+print("======================================")
+print("  WELCOME TO THE NUMBER GUESSING GAME  ")
+print("======================================\n")
+
+top_of_range = input("Type a Number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Please type in a number greater than 0 next time")
+        quit()
+else:
+    print("Please type in a number next time")
+    quit()
+
+random_number = random.randint(0, top_of_range)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type in a number next time")
+        continue
+
+    if user_guess == random_number:
+        print("You Got it!")
+        break
+    elif user_guess > random_number:
+        print("You were above the number!")
+    else:
+        print("You were below the number!")
+
+print(f"You got it in {guesses} guesses")
